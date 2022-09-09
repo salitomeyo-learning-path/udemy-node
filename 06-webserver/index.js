1ec3e0c3a45da9fb049ef23f1376a10c24e9cd6a
Salome Aristizabal Giraldo <salitomeyo16@gmail.com>
2022-09-09 13:13:48 -0500

    feat: serve a web page with express

--- 06-webserver/index.js
+++ 06-webserver/index.js
@@ -1,8 +1,21 @@
 const express = require('express')
-const app = express()
+const app = express();
+const port = 8080;
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.use( express.static('public') );
+
+app.get('/', (req, res) => {
+  res.send('This is the home reques')
 })
 
-app.listen(8080)
+app.get('/hello-world', (req, res) => {
+    res.send('this is the Hello World')
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/404.html')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
